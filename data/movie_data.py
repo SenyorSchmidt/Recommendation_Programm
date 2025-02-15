e398318cf86c3b555ad614d9d68c702c41e57884
SenyorSchmidt <155657963+SenyorSchmidt@users.noreply.github.com>
2025-02-15 17:34:39 +0100

    Setup of Programm

---
+++ data/movie_data.py
@@ -0,0 +1,6 @@
+# dict of movies that follows basic structure:
+# name of movie : [genres],[actors],[rating],[short description of story]
+
+movies = {
+    "Saving Private Ryan": ["action", "war", ]
+}
