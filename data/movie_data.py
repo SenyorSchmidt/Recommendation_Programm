ef7c9e151c8bf9a7895fa78725ebbe06d1137498
SenyorSchmidt <155657963+SenyorSchmidt@users.noreply.github.com>
2025-02-15 18:43:44 +0100

    Updating Movielist

--- data/movie_data.py
+++ data/movie_data.py
@@ -1,6 +1,77 @@
 # dict of movies that follows basic structure:
-# name of movie : [genres],[actors],[rating],[short description of story]
+# name of movie : [genres],[actors],[short description of story]
 
 movies = {
-    "Saving Private Ryan": ["action", "war", ]
+    "Saving Private Ryan": 
+        [
+            ["action", "war", "drama"], 
+            ["Tom Hanks", "Matt Damon", "Edward Burns", "Gary Lecinsohn"], 
+            ["Saving Private Ryan is a 1998 American epic war film directed by Steven Spielberg and written by Robert Rodat. Set in 1944 in Normandy, France, during World War II, it follows a group of soldiers, led by Captain John Miller (Tom Hanks), on a mission to locate Private James Francis Ryan (Matt Damon) and bring him home safely after his three brothers have been killed in action."]
+        ],
+    "Shawshank Redemption": 
+        [
+            ["drama", "action"],
+            ["Tim Robbins", "Morgan Freeman", "Bub Gunton", "William Sadler"],
+            ["Chronicles the experiences of a formerly successful banker as a prisoner in the gloomy jailhouse of Shawshank after being found guilty of a crime he did not commit. The film portrays the man's unique way of dealing with his new, torturous life; along the way he befriends a number of fellow prisoners, most notably a wise long-term inmate named Red"]
+        ],
+    "The Godfather":
+        [
+            ["drama", "mafia", "thriller", "gangster"],
+            ["Marlon Brando", "Al Pacino", "James Caan", "Diane Keaton"],
+            ["The Godfather \"Don\" Vito Corleone is the head of the Corleone mafia family in New York. He is at the event of his daughter's wedding. Michael, Vito's youngest son and a decorated WWII Marine is also present at the wedding. Michael seems to be uninterested in being a part of the family business. Vito is a powerful man, and is kind to all those who give him respect but is ruthless against those who do not. But when a powerful and treacherous rival wants to sell drugs and needs the Don's influence for the same, Vito refuses to do it. What follows is a clash between Vito's fading old values and the new ways which may cause Michael to do the thing he was most reluctant in doing and wage a mob war against all the other mafia families which could tear the Corleone family apart."]
+        ],
+            "Saving Private Ryan": 
+        [
+            ["action", "war", "drama"], 
+            ["Tom Hanks", "Matt Damon", "Edward Burns", "Gary Lecinsohn"], 
+            ["Saving Private Ryan is a 1998 American epic war film directed by Steven Spielberg and written by Robert Rodat. Set in 1944 in Normandy, France, during World War II, it follows a group of soldiers, led by Captain John Miller (Tom Hanks), on a mission to locate Private James Francis Ryan (Matt Damon) and bring him home safely after his three brothers have been killed in action."]
+        ],
+    "Shawshank Redemption": 
+        [
+            ["drama", "action"],
+            ["Tim Robbins", "Morgan Freeman", "Bub Gunton", "William Sadler"],
+            ["Chronicles the experiences of a formerly successful banker as a prisoner in the gloomy jailhouse of Shawshank after being found guilty of a crime he did not commit. The film portrays the man's unique way of dealing with his new, torturous life; along the way he befriends a number of fellow prisoners, most notably a wise long-term inmate named Red"]
+        ],
+    "The Godfather":
+        [
+            [
+                
+            ]
+        ],
+            "Saving Private Ryan": 
+        [
+            ["action", "war", "drama"], 
+            ["Tom Hanks", "Matt Damon", "Edward Burns", "Gary Lecinsohn"], 
+            ["Saving Private Ryan is a 1998 American epic war film directed by Steven Spielberg and written by Robert Rodat. Set in 1944 in Normandy, France, during World War II, it follows a group of soldiers, led by Captain John Miller (Tom Hanks), on a mission to locate Private James Francis Ryan (Matt Damon) and bring him home safely after his three brothers have been killed in action."]
+        ],
+    "Shawshank Redemption": 
+        [
+            ["drama", "action"],
+            ["Tim Robbins", "Morgan Freeman", "Bub Gunton", "William Sadler"],
+            ["Chronicles the experiences of a formerly successful banker as a prisoner in the gloomy jailhouse of Shawshank after being found guilty of a crime he did not commit. The film portrays the man's unique way of dealing with his new, torturous life; along the way he befriends a number of fellow prisoners, most notably a wise long-term inmate named Red"]
+        ],
+    "The Godfather":
+        [
+            [
+                
+            ]
+        ],
+            "Saving Private Ryan": 
+        [
+            ["action", "war", "drama"], 
+            ["Tom Hanks", "Matt Damon", "Edward Burns", "Gary Lecinsohn"], 
+            ["Saving Private Ryan is a 1998 American epic war film directed by Steven Spielberg and written by Robert Rodat. Set in 1944 in Normandy, France, during World War II, it follows a group of soldiers, led by Captain John Miller (Tom Hanks), on a mission to locate Private James Francis Ryan (Matt Damon) and bring him home safely after his three brothers have been killed in action."]
+        ],
+    "Shawshank Redemption": 
+        [
+            ["drama", "action"],
+            ["Tim Robbins", "Morgan Freeman", "Bub Gunton", "William Sadler"],
+            ["Chronicles the experiences of a formerly successful banker as a prisoner in the gloomy jailhouse of Shawshank after being found guilty of a crime he did not commit. The film portrays the man's unique way of dealing with his new, torturous life; along the way he befriends a number of fellow prisoners, most notably a wise long-term inmate named Red"]
+        ],
+    "The Godfather":
+        [
+            [
+                
+            ]
+        ]
 }
